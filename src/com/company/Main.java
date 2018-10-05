3935f86b62687645403917bec588fbb0c6499828
soplinger <so367@pvsd.org>
2018-10-05 11:19:39 -0400

    WHAT JUST HAPPENED?!?!?!!?!?!?!!

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Student Winston = new Student(12.1000101, "Winston");
+        Student Jake = new Student(8.9, "Jake");
+        Student Bennett = new Student(3.2, "Bennett");
+        Student Brian = new Student(3.4, "Brian");
+        Student Pennebacker = new Student(10000, "Pennebacker");
+
+
+        Vector<Student> st = new Vector<>()
+        st.add(Winston);
+        st.add(Jake);
+        st.add(Bennett);
+        st.add(Brian);
+        st.add(Pennebacker);
+
+        for (int i = 0; i < 3; i++){
+            double HighestGPA = 0.0;
+
+            if ()
+
+
+        }
+
+
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by so367 on 10/5/18.
+ */
+public class Student {
+    String name;
+    double gpa;
+
+    public Student (double g, String n) {
+
+        this.name = n;
+        this.gpa = g;
+
+        }
+
+}
