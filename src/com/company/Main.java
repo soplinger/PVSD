7cc7e0ae5d231c670e296e125012c66eb8704e1f
soplinger <solekibbles4@gmail.com>
2018-10-08 18:47:05 -0400

    this made me sad this led to me not eating cheerios

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,47 @@
 package com.company;
 
-import java.util.Vector;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Student Winston = new Student(12.1000101, "Winston");
-        Student Jake = new Student(8.9, "Jake");
+        Student Winston = new Student(2.1, "Winston");
+        Student Jake = new Student(1.2, "Jake");
         Student Bennett = new Student(3.2, "Bennett");
         Student Brian = new Student(3.4, "Brian");
         Student Pennebacker = new Student(10000, "Pennebacker");
 
 
-        Vector<Student> st = new Vector<>()
+        Vector<Student> st = new Vector<>();
         st.add(Winston);
         st.add(Jake);
         st.add(Bennett);
         st.add(Brian);
         st.add(Pennebacker);
 
-        for (int i = 0; i < 3; i++){
-            double HighestGPA = 0.0;
+        Vector<Student> TopStudents = new Vector<>();
 
-            if ()
+        for (int i = 0; i < 3; i++) {
+            double highestGPA = 0.0;
+            Student highestStudent = null;
+
+            for (int a = 0; a < st.size(); a++) {
+
+
+                if (st.get(a).gpa > highestGPA) ;
+                {
+                    highestGPA = st.get(a).gpa;
+                    highestStudent = st.get(a);
+
+                }
+            }
+            TopStudents.add(highestStudent);
+            st.remove(highestStudent);
+        }
+
+           for (Student hs : TopStudents){
+               System.out.println(hs.name + " | " + hs.gpa);
 
 
         }
